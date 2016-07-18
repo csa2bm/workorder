59e7c31980a928b7e61a66b6d26a944bb6ccd790
mmm2bm <mmm@2bm.com>
2016-07-18 12:51:03 +0000

    New blocks

--- webapp/components/workOrderDetails/blocks/OperationsBlock.js
+++ webapp/components/workOrderDetails/blocks/OperationsBlock.js
@@ -1,12 +1,12 @@
-sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.OperationsBlock", {
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.AttachmentsBlock", {
     metadata: {
         views: {
             Collapsed: {
-                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.OperationsBlock",
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.AttachmentsBlock",
                 type: "XML"
             },
             Expanded: {
-                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.OperationsBlock",
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.AttachmentsBlock",
                 type: "XML"
             }
         }
