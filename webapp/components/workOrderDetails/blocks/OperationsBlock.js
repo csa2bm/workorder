7547cb08627f36e3e9f4e692b9678c4ca4b37308
Sujeevan Kuganesan <suj@2bm.com>
2016-07-18 13:09:47 +0000

    Fixed more block issues

--- webapp/components/workOrderDetails/blocks/OperationsBlock.js
+++ webapp/components/workOrderDetails/blocks/OperationsBlock.js
@@ -1,12 +1,12 @@
-sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.AttachmentsBlock", {
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.OperationsBlock", {
     metadata: {
         views: {
             Collapsed: {
-                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.AttachmentsBlock",
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.OperationsBlock",
                 type: "XML"
             },
             Expanded: {
-                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.AttachmentsBlock",
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.OperationsBlock",
                 type: "XML"
             }
         }
