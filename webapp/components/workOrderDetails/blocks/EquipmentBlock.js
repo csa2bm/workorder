29659eb4128b788ad7974db54f0370e3d0295cc1
Sujeevan Kuganesan <suj@2bm.com>
2016-07-19 11:10:43 +0000

    Added equipmentblock to workorderdetail view incl. binding

---
+++ webapp/components/workOrderDetails/blocks/EquipmentBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.EquipmentBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.EquipmentBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.EquipmentBlock",
+                type: "XML"
+            }
+        }
+    }
+});
