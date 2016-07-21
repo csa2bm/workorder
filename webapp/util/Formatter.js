9dfbe3cf21fdd32ae114993cb33e5643afa90b16
Sujeevan Kuganesan <suj@2bm.com>
2016-07-21 08:01:59 +0000

    Added expanding so data gets loaded and working on devices

--- webapp/util/Formatter.js
+++ webapp/util/Formatter.js
@@ -14,7 +14,7 @@ sap.ui.define([], function() {
 		/**
 		 * Returns false if a string is empty
 		 * param{String} sStr input string
-		 * @returns {Boolean}} the input string with the first uppercase character
+		 * @returns {Boolean}} false if the input string is empty
 		 */
 		isNotEmptyStr: function(sStr) {
 			if (!sStr || sStr === "") {
@@ -23,7 +23,12 @@ sap.ui.define([], function() {
 				return true;
 			}
 		},
-
+		
+		/**
+		 * If operationStatus is true use icon
+		 * param{Boolean} sStatus input string
+		 * @returns {String}} for SAP icon
+		 */
 		operationStatus: function(sStatus) {
 			if (sStatus) {
 				return "sap-icon://accept";
@@ -31,13 +36,30 @@ sap.ui.define([], function() {
 				return ""; // "sap-icon://message-warning";
 			}
 		},
-
+		
+		/**
+		 * Based on the value of operationstatus use different colors
+		 * param{Boolean} sStatus input string
+		 * @returns {String}} for SAP icon color
+		 */
 		opStatusColor: function(sStatus) {
 			if (sStatus) {
 				return "Green";
 			} else {
 				return "Red";
 			}
-		}
+		},
+		
+		/**
+		 * Convert type date to the format of 01/02/2017
+		 * param{Date}  input string
+		 * @returns {Date}} with the corresponding format
+		 */
+		dateTimeShortType: function() {
+		var dateType = new sap.ui.model.type.Date({
+			pattern: "dd/MM/yyyy"
+		});
+		return dateType;
+	}
 	};
 });
