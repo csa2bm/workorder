49b22c8de64f089ea06ff164d99d5be02c49b9c1
Sujeevan Kuganesan <suj@2bm.com>
2016-07-26 07:30:01 +0000

    Added ComponentBloick incl. translation

---
+++ webapp/components/workOrderDetails/blocks/ComponentsBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.ComponentsBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.ComponentsBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.ComponentsBlock",
+                type: "XML"
+            }
+        }
+    }
+});
