66eae2af596d4f3c0721bd037e740b849dc41f75
Sujeevan Kuganesan <suj@2bm.com>
2016-07-29 12:10:29 +0000

    +Added status change button - missing post status change to DB

--- webapp/components/workOrderDetails/WorkOrderDetails.controller.js
+++ webapp/components/workOrderDetails/WorkOrderDetails.controller.js
@@ -92,6 +92,97 @@ sap.ui.define([
 				var oRouter = this.getRouter();
 				oRouter.navTo("workOrderList", true);
 			}	
+		},
+		
+		orderStatusBtnPressed: function(oEvent) {
+			//var oButton = oEvent.getSource();
+			var oContext = this.getView().getBindingContext();
+			var OrderStatus = this.getView().getModel().getProperty("orderStatus", oContext);
+			OrderStatus = "inProgress";
+			this.setUserStatusTaskStarted(OrderStatus);
+			
+			/*
+
+			// create menu only once
+			if (!this._orderStatusMenu) {
+				this._orderStatusMenu = sap.ui.xmlfragment("com.twobm.mobileworkorder.components.workOrderDetails.controls.OrderStatusChangeMenu", this);
+				this.getView().addDependent(this._orderStatusMenu);
+			}
+
+			//var eDock = sap.ui.core.Popup.Dock;
+			this._orderStatusMenu.openBy(oButton); //.open(this._bKeyboard, oButton, eDock.BeginTop, eDock.BeginBottom, oButton);
+			*/
+		},
+		
+		setUserStatusTaskStarted: function(orderStatus) {
+			var message = this.getI18nText("orderStatusMessage" + orderStatus);
+			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			var that = this;
+			sap.m.MessageBox.show(message, {
+				icon: sap.m.MessageBox.Icon.None,
+				title: this.getI18nText("orderStatusTitle"),
+				actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+				defaultAction: sap.m.MessageBox.Action.NO,
+				styleClass: bCompact ? "sapUiSizeCompact" : "",
+				onClose: function(oAction, object) {
+					if (oAction === sap.m.MessageBox.Action.YES) {
+						// Set the order status to "inProgress" and post it
+						if (orderStatus === that.getI18nText("orderStatusNotStarted")) {
+							//that.onNavBack();
+							console.log("Order status: Not started");
+						}
+						// Set the order status to "completed" and post it
+						else if(orderStatus === that.getI18nText("orderStatusInProgress")){
+							console.log("Order status: In progress");
+						}
+						
+						else {
+							return;
+							/*
+							var requiredMessage = that.getI18nText("messageBoxTextRequiredChecklistsNotCompleted");
+
+							sap.m.MessageBox.show(requiredMessage, {
+								icon: sap.m.MessageBox.Icon.INFORMATION,
+								title: that.getI18nText("messageBoxHeaderRequiredChecklistsNotCompleted"),
+								actions: [sap.m.MessageBox.Action.OK],
+								defaultAction: sap.m.MessageBox.Action.OK,
+								styleClass: bCompact ? "sapUiSizeCompact" : ""
+							});
+							*/
+						}
+						var oContext = that.getView().getBindingContext();
+						that.getView().getModel().setProperty("OrderStatus", orderStatus, oContext);
+						
+					}
+				}
+			});
+
+		},
+		getOrderStatusBtnText: function(sString){
+			var btnText = this.getI18nText("orderStatusBtnTextNotStarted");
+			if(sString === this.getI18nText("orderStatusNotStarted")){
+				btnText = this.getI18nText("orderStatusBtnTextNotStarted");
+			}
+			else if(sString === this.getI18nText("orderStatusInProgress")){
+				btnText = this.getI18nText("orderStatusBtnTextInProgress");
+			}/*
+			else if(sString === this.getI18nText("orderStatusCompleted")){
+				btnText = this.getI18nText("orderStatusBtnTextNotStarted");
+			}
+			*/
+			else{
+					btnText = this.getI18nText("orderStatusBtnTextCompleted");
+			}
+			return btnText;
+		},
+		isOrderNotCompleted: function(sString){
+			sString="complete";
+			if(sString === this.getI18nText("orderStatusCompleted")){
+				return false;
+			}
+			else{
+				return true;
+			}
 		}
 
 	});
