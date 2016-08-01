8300743b9ac4e374fce133e95411ff828d92ab3f
Sujeevan Kuganesan <suj@2bm.com>
2016-08-01 13:52:33 +0000

    Edit longText is now implemented

--- webapp/components/workOrderDetails/NotesBlock.controller.js
+++ webapp/components/workOrderDetails/NotesBlock.controller.js
@@ -8,17 +8,55 @@ sap.ui.define([
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.NotesBlock
+		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.AttachmentsBlock
 		 */
-			onInit: function() {
-				this._showFormFragment("LongTextFragmentDisplay");
-		
-			},
+		onInit: function() {
+			this._showFormFragment("LongTextFragmentDisplay");
+
+		},
+
+		editLongText: function() {
+			this.toggleButtonsAndView(true);
+		},
+
+		SaveLongText: function() {
+
+			var orderNo = this.getView().getBindingContext().getObject().Orderid;
+			var that = this;
+
+			var parameters = {
+				success: function(oData, response) {
+
+				},
+				error: that.errorCallBackShowInPopUp
+			};
+
+			var dataUpdate = {
+				LongText: this.getView().getBindingContext().getObject().LongText
+			};
+
+			var updatePath = "/OrderSet(Orderid='" + orderNo + "')";
+
+			this.getView().getModel().update(updatePath, dataUpdate, parameters);
+
+			that.toggleButtonsAndView(false);
+		},
+
+		toggleButtonsAndView: function(bEdit) {
+			var oView = this.getView();
+
+			// Show the appropriate action buttons
+			oView.byId("editButton").setVisible(!bEdit);
+			oView.byId("saveButton").setVisible(bEdit);
+
+			// Set the right form type
+			this._showFormFragment(bEdit ? "LongTextFragmentChange" : "LongTextFragmentDisplay");
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.NotesBlock
+		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.AttachmentsBlock
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -27,7 +65,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.
+		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.AttachmentsBlock
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -35,39 +73,39 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.
+		 * @memberOf com.twobm.mobileworkorder.components.workOrderDetails.view.AttachmentsBlock
 		 */
-		onExit : function () {
-			for(var sPropertyName in this._formFragments) {
-				if(!this._formFragments.hasOwnProperty(sPropertyName)) {
+		onExit: function() {
+			for (var sPropertyName in this._formFragments) {
+				if (!this._formFragments.hasOwnProperty(sPropertyName)) {
 					return;
 				}
- 
+
 				this._formFragments[sPropertyName].destroy();
 				this._formFragments[sPropertyName] = null;
 			}
 		},
-		
+
 		_formFragments: {},
-		
-		_getFormFragment: function (sFragmentName) {
+
+		_getFormFragment: function(sFragmentName) {
 			var oFormFragment = this._formFragments[sFragmentName];
- 
+
 			if (oFormFragment) {
 				return oFormFragment;
 			}
- 
-			//oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "com.twobm.mobileworkorder.components.workOrderDetails.fragments." + sFragmentName);
-			 oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "com.twobm.mobileworkorder.components.workOrderDetails.fragments.LongTextFragmentDisplay");
+
+			oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "com.twobm.mobileworkorder.components.workOrderDetails.fragments." +
+				sFragmentName);
+
 			return this._formFragments[sFragmentName] = oFormFragment;
 		},
-		
-		_showFormFragment : function (sFragmentName) {
-			var vLayout = this.getView().byId("layout1");
- 
-			//oPage.removeAllContent();
-			var oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "com.twobm.mobileworkorder.components.workOrderDetails.fragments.LongTextFragmentDisplay");
-			vLayout.insertContent(oFormFragment);
+
+		_showFormFragment: function(sFragmentName) {
+			var oPage = this.getView().byId("NotesLayoutId");
+
+			oPage.removeAllContent();
+			oPage.insertContent(this._getFormFragment(sFragmentName));
 		}
 
 	});
