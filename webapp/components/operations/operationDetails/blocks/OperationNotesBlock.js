c77dcd6b473981f5b256efafa77a58284ea77053
Sujeevan Kuganesan <suj@2bm.com>
2016-08-05 09:20:44 +0000

    Added OperationDetailsBlocks

---
+++ webapp/components/operations/operationDetails/blocks/OperationNotesBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.operations.operationDetails.blocks.OperationNotesBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.operations.operationDetails.OperationNotesBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.operations.operationDetails.OperationNotesBlock",
+                type: "XML"
+            }
+        }
+    }
+});
