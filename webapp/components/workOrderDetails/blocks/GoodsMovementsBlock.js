1a1e78690f08e076a517433779003e233d5765e6
mmm2bm <mmm@2bm.com>
2016-08-26 07:32:18 +0000

    Start on goods issue

---
+++ webapp/components/workOrderDetails/blocks/GoodsMovementsBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.GoodsMovementsBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.GoodsMovementsBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.GoodsMovementsBlock",
+                type: "XML"
+            }
+        }
+    }
+});
