45d26963e4138defd68089c7aa1e0ff796d1cf57
Sujeevan Kuganesan <suj@2bm.com>
2016-10-03 10:36:42 +0000

    Translation and column defined

---
+++ webapp/components/measuringPointDetails/blocks/MeasurementHistoryBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.measuringPointDetails.blocks.MeasurementHistoryBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.measuringPointDetails.MeasurementHistoryBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.measuringPointDetails.MeasurementHistoryBlock",
+                type: "XML"
+            }
+        }
+    }
+});
