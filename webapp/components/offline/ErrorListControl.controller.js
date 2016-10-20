80dc85986e2ad813b87461f3937353f2e2cd0bcc
mmm2bm <mmm@2bm.com>
2016-10-20 21:16:25 +0000

    Delete error working

--- webapp/components/offline/ErrorListControl.controller.js
+++ webapp/components/offline/ErrorListControl.controller.js
@@ -6,8 +6,9 @@ sap.ui.define([
 	return Controller.extend("com.twobm.mobileworkorder.components.offline.ErrorListControl", {
 		openErrorsView: function(oEvent) {
 			if (!this._errorsView) {
-				
-				this._errorsView = sap.ui.xmlfragment("errorArchiveDialog", "com.twobm.mobileworkorder.components.offline.fragments.ErrorsListPopover", this);
+
+				this._errorsView = sap.ui.xmlfragment("errorArchiveDialog",
+					"com.twobm.mobileworkorder.components.offline.fragments.ErrorsListPopover", this);
 				this.getView().addDependent(this._errorsView);
 			}
 
@@ -53,7 +54,21 @@ sap.ui.define([
 			var deletePath = oEvent.getSource().getBindingContext("syncStatusModel").getObject().ErrorUrl;
 			var that = this;
 			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
-			
+
+			// var serviceUrl = sap.ui.getCore().getComponent("__component0").getModel().sServiceUrl;
+			// var errorsUrl = serviceUrl + "/ErrorArchive";
+
+			// var request = {
+			// 	headers: {},
+			// 	requestUri: errorsUrl,
+			// 	method: "GET"
+			// };
+
+			// OData.read(request,
+			// 	function(data, response) {
+
+			// 	});
+
 			sap.m.MessageBox.show(this.getI18nText("errorListPopupDetailDeletePopupText"), {
 				icon: sap.m.MessageBox.Icon.None,
 				title: this.getI18nText("errorListPopupDetailDeletePopupTitle"),
@@ -61,24 +76,39 @@ sap.ui.define([
 				defaultAction: sap.m.MessageBox.Action.NO,
 				styleClass: bCompact ? "sapUiSizeCompact" : "",
 				onClose: function(oAction, object) {
-					
-					if (oAction === sap.m.MessageBox.Action.YES) {
 
+					if (oAction === sap.m.MessageBox.Action.YES) {
 						var deleteErrorsURL = deletePath.replace(that.getView().getModel().sServiceUrl, "");
-						sap.m.MessageBox.show(deleteErrorsURL);
-						var parameters = {
-							success: function(evt) {
-									sap.m.MessageBox.show("3");
-								
+						
+						var request = {
+							headers: {},
+							requestUri: deletePath,
+							method: "DELETE"
+						};
+
+						OData.read(request,
+							function(data, response) {
 								var oNavCon = sap.ui.core.Fragment.byId("errorArchiveDialog", "errorNav");
 								oNavCon.back();
+								
+								//that.getEventBus().publish("UpdateSyncState");
+								that.getEventBus().publish("OfflineStore", "Synced");
+							}, this.errorCallBackShowInPopUp);
 
-								that.getEventBus().publish("UpdateSyncState");
-							},
-							error: that.errorCallBackShowInPopUp
-						};
-						sap.m.MessageBox.show(that.getView().getModel().sServiceUrl);
-						that.getView().getModel().remove(deleteErrorsURL, parameters);
+						// var deleteErrorsURL = deletePath.replace(that.getView().getModel().sServiceUrl, "");
+						// sap.m.MessageBox.show(deleteErrorsURL);
+						// var parameters = {
+						// 	success: function(evt) {
+
+						// 		var oNavCon = sap.ui.core.Fragment.byId("errorArchiveDialog", "errorNav");
+						// 		oNavCon.back();
+
+						// 		that.getEventBus().publish("UpdateSyncState");
+						// 	},
+						// 	error: that.errorCallBackShowInPopUp
+						// };
+						// sap.m.MessageBox.show(that.getView().getModel().sServiceUrl);
+						// that.getView().getModel().remove(deleteErrorsURL, parameters);
 					}
 				}
 			});
