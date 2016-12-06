0d28d1eec0310a234ea5a0c5f79974fd7e1b7419
Thomas Kristjansson <tkr@2bm.com>
2016-12-06 10:06:17 +0000

    Revert "XML instead of JSON"
    This reverts commit d56bdec6f04fada293275a8a43ce0f6602afdf3d.

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,7 +20,7 @@ sap.ui.define([
 			var oModel, appContext, sServiceUrl;
 			//create odata model for kapsel application
 			var param = {
-				"json": false,
+				"json": true,
 				loadMetadataAsync: true,
 				useBatch: true,
 				refreshAfterChange: true
