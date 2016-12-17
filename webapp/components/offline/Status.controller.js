5afcd49bf5adf0bacfa98d450d656bf578bd566b
Olav Rask <ora@2bm.com>
2016-12-17 10:54:52 +0000

    Basic reset function

--- webapp/components/offline/Status.controller.js
+++ webapp/components/offline/Status.controller.js
@@ -36,28 +36,36 @@ sap.ui.define([
 			}
 		},
 
-		resetStore: function() {
-			var that = this;
-
-			this._syncQuickView.close();
-
-			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
-			sap.m.MessageBox.show("Are you sure that you want to reset the offline database and login again?", {
-				icon: sap.m.MessageBox.Icon.None,
-				title: "Reset database",
-				actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
-				defaultAction: sap.m.MessageBox.Action.NO,
-				styleClass: bCompact ? "sapUiSizeCompact" : "",
-				onClose: function(oAction, object) {
-					if (oAction === sap.m.MessageBox.Action.YES) {
-						that.closeSyncPopup();
-						sap.ui.getCore().byId("appShell").setVisible(false); // hide app with data
-						sap.m.MessageToast.show("Resetting data and logging out");
-						DevApp.devLogon.reset();
-					}
-				}
-			});
-		},
+	     resetStore: function() {
+                             var that = this;
+                             
+                             this._syncQuickView.close();
+                             
+                             var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+                             sap.m.MessageBox.show("Are you sure that you want to reset the offline database and login again?", {
+                                                   icon: sap.m.MessageBox.Icon.None,
+                                                   title: "Reset database",
+                                                   actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+                                                   defaultAction: sap.m.MessageBox.Action.NO,
+                                                   styleClass: bCompact ? "sapUiSizeCompact" : "",
+                                                   onClose: function(oAction, object) {
+                                                   if (oAction === sap.m.MessageBox.Action.YES) {
+                                                   that.closeSyncPopup();
+                                                   /*sap.ui.getCore().byId("appShell").setVisible(false); // hide app with data
+                                                    sap.m.MessageToast.show("Resetting data and logging out");
+                                                    DevApp.devLogon.reset();*/
+                                                   
+                                                   sap.hybrid.OData.offlineStore.appOfflineStore.store.close(function() {
+                                                                                                             
+                                                                                                             sap.OData.removeHttpClient();
+                                                                                                             sap.hybrid.OData.offlineStore.appOfflineStore.store.clear(function() {
+                                                                                                                                                                       sap.hybrid.OData.offlineStore.appOfflineStore.store = null;
+                                                                                                                                                                       sap.hybrid.kapsel.doDeleteRegistration();                                                           });
+                                                                                                             });
+                                                   }
+                                                   }
+                                                   });
+                             },
 		
 		showErrorsList : function(){
 			var data = {
