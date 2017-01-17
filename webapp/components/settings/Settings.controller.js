e420255bf313b333d159760e17e13458afa7a790
Morten Milbak Mortensen <mmm@2bm.com>
2017-01-17 13:53:04 +0000

    Reset of device working

--- webapp/components/settings/Settings.controller.js
+++ webapp/components/settings/Settings.controller.js
@@ -11,7 +11,38 @@ sap.ui.define([
 
 		onResetClient : function()
 		{
-			sap.m.MessageToast.show("test");
+			//sap.m.MessageToast.show("test");
+			
+			var that = this;
+
+			//this._syncQuickView.close();
+
+			//var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			sap.m.MessageBox.show("Are you sure that you want to reset the offline database and login again?", {
+				icon: sap.m.MessageBox.Icon.None,
+				title: "Reset database",
+				actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+				defaultAction: sap.m.MessageBox.Action.NO,
+				//styleClass: bCompact ? "sapUiSizeCompact" : "",
+				onClose: function(oAction, object) {
+					if (oAction === sap.m.MessageBox.Action.YES) {
+						//that.closeSyncPopup();
+						/*sap.ui.getCore().byId("appShell").setVisible(false); // hide app with data
+						 sap.m.MessageToast.show("Resetting data and logging out");
+						 DevApp.devLogon.reset();*/
+
+						sap.hybrid.OData.offlineStore.appOfflineStore.store.close(function() {
+
+							sap.OData.removeHttpClient();
+							sap.hybrid.OData.offlineStore.appOfflineStore.store.clear(function() {
+								sap.hybrid.OData.offlineStore.appOfflineStore.store = null;
+								sap.hybrid.kapsel.doDeleteRegistration();
+								sap.Logon.core.loadStartPage();
+							});
+						});
+					}
+				}
+			});
 		}
 	});
 });
