14f0cf1268e60ce4fd83e8f283a2fa5e34ce33e0
Morten Milbak Mortensen <mmm@2bm.com>
2017-01-23 11:26:56 +0000

    Added documents block

---
+++ webapp/components/workOrderDetails/blocks/configuration/DocumentsBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.configuration.DocumentsBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.blocks.DocumentsBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.blocks.DocumentsBlock",
+                type: "XML"
+            }
+        }
+    }
+});
