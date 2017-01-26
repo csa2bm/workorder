03ec61ce3970cbca1927fde6ddfcf3a5308cac06
Kuganesan Sujeevan <suj@2bm.com>
2017-01-26 14:53:27 +0000

    Data showing now for itemDetails task and acitivity block

---
+++ webapp/components/itemDetails/blocks/configuration/ActivitiesBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.itemDetails.blocks.configuration.ActivitiesBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.itemDetails.blocks.ActivitiesBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.itemDetails.blocks.ActivitiesBlock",
+                type: "XML"
+            }
+        }
+    }
+});
---
+++ webapp/components/itemDetails/blocks/configuration/TasksBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.itemDetails.blocks.configuration.TasksBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.itemDetails.blocks.TasksBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.itemDetails.blocks.TasksBlock",
+                type: "XML"
+            }
+        }
+    }
+});
