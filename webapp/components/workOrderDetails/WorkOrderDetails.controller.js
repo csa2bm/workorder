12436b97113fcfd2df53051ce654fc6828ff8222
Olav Rask <ora@2bm.com>
2017-02-07 12:39:21 +0000

    Merged changes

--- webapp/components/workOrderDetails/WorkOrderDetails.controller.js
+++ webapp/components/workOrderDetails/WorkOrderDetails.controller.js
@@ -398,8 +398,7 @@ sap.ui.define([
 			return false;
 		},
 
-<<<<<<< HEAD
-		
+
 			searchEmployeePress: function(oEvent) {
 			var sValue = oEvent.getParameter("query");
 			var searchString = sValue.toLowerCase();
@@ -442,8 +441,6 @@ sap.ui.define([
 		},
 
 
-=======
->>>>>>> branch 'master' of https://t0PPy@bitbucket.org/2bm/com.twobm.mobileworkorder.git
 		onTimeRegistrationTimerChangeButtonPressed: function(oEvent) {
 			var timerModel = this.getView().getModel("timeRegistrationTimerModel").getData();
 
