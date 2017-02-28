fd8f5edfb06d683f89e1a17ab4eae0ca3d408a74
Morten Milbak Mortensen <mmm@2bm.com>
2017-02-28 08:26:55 +0000

    Renamed attachmentsblock to photosblock

---
+++ webapp/components/workOrderDetails/blocks/configuration/PhotosBlock.js
@@ -0,0 +1,14 @@
+sap.uxap.BlockBase.extend("com.twobm.mobileworkorder.components.workOrderDetails.blocks.configuration.PhotosBlock", {
+    metadata: {
+        views: {
+            Collapsed: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.blocks.PhotosBlock",
+                type: "XML"
+            },
+            Expanded: {
+                viewName: "com.twobm.mobileworkorder.components.workOrderDetails.blocks.PhotosBlock",
+                type: "XML"
+            }
+        }
+    }
+});
